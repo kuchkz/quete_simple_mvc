2d5403c1bb91dc75b69e53d736493ce819f33e34
Christophe Kuchmac <christophekuchmac@free.fr>
2018-10-01 21:51:24 +0200

    Adding files for quete2.

--- public/index.php
+++ public/index.php
@@ -1,35 +1,9 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Simple MVC</title>
-
-    <link rel="stylesheet" href="/assets/css/styles.css">
-    <link rel="icon" href="/assets/images/favicon.png">
-</head>
-
 <?php
-// connection BDD (définir les constantes dans un fichier app.php qui sera inclus)
-require '../app/db.php';
-$pdo = new \PDO(DSN, USER, PASS);
+// chargement de l'autoload en début de fichier
+use Controller\ItemController;
+require __DIR__ . '/../vendor/autoload.php';
 
-// requete SQL 
-$query = "SELECT * FROM item";
-$res = $pdo->query($query);
-$items = $res->fetchAll();
+$viewresult = new ItemController();
+$viewresult->index();
+//...
 ?>
-
-<!-- affichage des items récupérés par la requête SQL -->
-<body>
-    <section>
-        <h1>Items</h1>
-        <ul>
-        <?php foreach ($items as $item) : ?>
-            <li><?= $item['title'] ?></li>
-        <?php endforeach ?>
-        </ul>
-    </section>
-</body>
-</html>
---
+++ src/Controller/ItemController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Controller;
+
+// src/Controller/ItemController.php
+use Model\ItemManager;
+
+
+class ItemController
+{
+    public function index(){
+        $ItemManager = new ItemManager();
+        $items = $ItemManager->selectAllItems();
+        require __DIR__ . '/../View/item.php';
+    }
+}
+?>
